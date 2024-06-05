e4e28fa9fabe96e199760bacf6247d82e8207cb4
Vladayslava Revenkova <vladyslavarevenkova@gmail.com>
2024-06-05 22:52:40 +0000

    add function isWinner that will show who won the round, taking into account the rules of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,37 @@ controlsButtons.forEach(
         });
 function makeSelection(selection) {
 console.log(selection);}
+
+function makeSelection(userChoice){
+    console.log('User choice:', userChoice);
+    const computerChoice = getRandomChoice();
+    console.log('Computer choice:', computerChoice);
+}
+
+function isWinner(userChoice, computerChoice){
+
+    const winningConditions = {
+        rock: ['scissors','lizard'],
+        paper: ['rock', 'spock'],
+        scissors: ['paper', 'lizard'],
+        lizard: ['spock', 'paper'],
+        spock: ['scissors', 'rock']
+    };
+    if(userChoice===computerChoice){
+        return 'tie';
+    }
+    else if (winningConditions[userChoice].includes(computerChoice)) {
+        return 'user';
+    }
+    else {
+        return 'computer';
+    }
+}
+
+function getRandomChoice() {
+    const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+
+
